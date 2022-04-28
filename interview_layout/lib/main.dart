0ed706066d6ea025c5851a1012f4d938b7931214
Mason Fibkins <mason.fibkins@gmail.com>
2022-04-27 20:20:00 -0600

    added picture and fixed path errors

--- interview_layout/lib/main.dart
+++ interview_layout/lib/main.dart
@@ -79,6 +79,12 @@ class MyApp extends StatelessWidget {
           title: const Text('Flutter layout demo'),
         ),
         body: Column(children: [
+          Image.asset(
+            'images/lake.jpeg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
           titleSection,
           buttonSection,
           textSection,
